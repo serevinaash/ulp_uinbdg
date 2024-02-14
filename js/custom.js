106face8c3cb246d72690744ebd631160fb14b1d
serevinaash <serevinasherly02@gmail.com>
2024-02-14 11:44:12 +0700

    home

---
+++ js/custom.js
@@ -0,0 +1,41 @@
+(function ($) {
+
+  "use strict";
+
+    // PRE LOADER
+    $(window).load(function(){
+      $('.preloader').fadeOut(1000); // set duration in brackets    
+    });
+
+
+
+
+    // Owl Carousel
+    $('.owl-carousel').owlCarousel({
+      animateOut: 'fadeOut',
+      items:1,
+      loop:true,
+      autoplay:true,
+    })
+
+
+    // PARALLAX EFFECT
+    $.stellar();  
+
+
+    // SMOOTHSCROLL
+    $(function() {
+      $('.navbar-default a, #home a, footer a').on('click', function(event) {
+        var $anchor = $(this);
+          $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 49
+          }, 1000);
+            event.preventDefault();
+      });
+    });  
+
+
+    // WOW ANIMATION
+    new WOW({ mobile: false }).init();
+
+})(jQuery);
